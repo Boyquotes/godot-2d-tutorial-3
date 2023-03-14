396ab4b13a5cbfc1bc70bc7c857c7be51a096d46
Erick Christian <erickchristian48@gmail.com>
2023-03-14 21:33:48 +0700

    feat: done main scene

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400
 var screen_size
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -39,3 +40,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = 'up'
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
